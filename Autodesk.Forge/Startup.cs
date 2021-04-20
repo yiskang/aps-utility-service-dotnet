c07464a2360a8c12df3e7a27e3a7fcce3596cad7
Eason Kang <eason.kang@autodesk.com>
2021-04-20 18:11:11 +0800

    Migrate to .net core 3.1

--- Autodesk.Forge/Startup.cs
+++ Autodesk.Forge/Startup.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using AspNetCore.Proxy;
+using AspNetCore.Proxy.Options;
 using Autodesk.Forge.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -65,7 +66,7 @@ namespace Autodesk.Forge
 
             services.AddSingleton<ForgeTokenService>();
             services.AddProxies();
-            services.AddMvc();
+            services.AddMvc().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,15 +78,16 @@ namespace Autodesk.Forge
             }
 
             app.UseCors(builder =>
-                builder.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials()
+                builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed(origin => true) // allow any origin
+                    .AllowCredentials()
             );
 
             var forgeConfig = forgeOpts.Value;
             var forgeURL = UriHelper.BuildAbsolute(forgeConfig.Scheme, forgeConfig.Host);
-            var proxyOpts = ProxyOptions.Instance
+            var proxyOpts = HttpProxyOptionsBuilder.Instance
                             .WithBeforeSend((context, message) =>
                             {
                                 var proxyService = context.RequestServices.GetRequiredService<ForgeTokenService>();
@@ -105,23 +107,58 @@ namespace Autodesk.Forge
                                 context.Response.StatusCode = 403;
                                 var result = new
                                 {
-                                    message = "Request cannot be proxyed",
-                                    resason = exception.ToString()
+                                    message = "Request cannot be proxied",
+                                    reason = exception.ToString()
                                 };
                                 await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
-                            });
+                            }).Build();
 
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
             var proxyPrefix = forgeConfig.ProxyUri + "/{**catchall}";
-            app.UseProxy(proxyPrefix, (context, args) => {
-                var queries = context.Request.QueryString;
-                if (queries.HasValue)
-                {
-                    return $"{forgeURL}/{args["catchall"]}{queries.Value}";
-                }
-                return $"{forgeURL}/{args["catchall"]}";
-            }, proxyOpts);
 
-            app.UseMvcWithDefaultRoute();
+            app.UseProxies(proxies =>
+            {
+                proxies.Map(proxyPrefix, proxy =>
+                {
+                    proxy.UseHttp((context, args) =>
+                    {
+                        var queries = context.Request.QueryString;
+                        if (queries.HasValue)
+                        {
+                            return $"{forgeURL}/{args["catchall"]}{queries.Value}";
+                        }
+                        return $"{forgeURL}/{args["catchall"]}";
+                    },
+                    builder =>
+                    {
+                        builder.WithBeforeSend((context, message) =>
+                        {
+                            var proxyService = context.RequestServices.GetRequiredService<ForgeTokenService>();
+                            var token = proxyService.Token;
+
+                            // Set something that is needed for the downstream endpoint.
+                            message.Headers.Add("X-Forwarded-Host", context.Request.Host.Host);
+                            message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+                            context.Response.Headers.Remove("Content-Type");
+                            context.Response.Headers.Append("Content-Type", "application/json; chartset=utf-8");
+
+                            return Task.CompletedTask;
+                        })
+                        .WithHandleFailure(async (context, exception) =>
+                        {
+                            // Return a custom error response.
+                            context.Response.StatusCode = 403;
+                            var result = new
+                            {
+                                message = "Request cannot be proxyed",
+                                reason = exception.ToString()
+                            };
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+                        });
+                    });
+                });
+            });
         }
     }
 }
